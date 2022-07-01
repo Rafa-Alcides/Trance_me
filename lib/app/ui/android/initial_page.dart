4b12b8f47874d6a1f981bbbbd4d3ddfca0d05328
Rafael Alcides <rafalcides@icloud.com>
2022-07-01 00:34:46 -0300

    Tela de splashScreen

--- lib/app/ui/android/initial_page.dart
+++ lib/app/ui/android/initial_page.dart
@@ -1,12 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class InitialPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:trance_me/app/ui/android/login_page.dart';
+import 'package:trance_me/app/ui/theme/app_colors.dart';
+
+class InitialPage extends StatefulWidget {
   const InitialPage({Key? key}) : super(key: key);
 
   @override
+  State<InitialPage> createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 5),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Initial Page")),
+    return Scaffold(
+      backgroundColor: AppColor.bgFieldCpf,
+      body: Center(
+        child: Image.asset("assets/logoo.png"),
+      ),
     );
   }
 }
