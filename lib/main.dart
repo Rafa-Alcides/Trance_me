f5413f22fbfcaaf5d26d20deebf7d6da8df5550a
Rafael Alcides <rafalcides@icloud.com>
2022-06-30 21:33:53 -0300

    restruturando pastas getx partenns

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:trance_me/app/theme/app_theme.dart';
 
 void main() {
-  runApp(GetMaterialApp(
+  runApp(const GetMaterialApp(
     title: "Trance_Me",
     debugShowCheckedModeBanner: false,
-    theme: appThemeData,
-    //initialRoute: ,
-    //getPages: [],
-    //initialBinding: ,
   ));
 }
